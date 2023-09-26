f5c18803dd597af400e2b4f776ab940000eca907
GhaAngga <ghaa.saptohadi17@gmail.com>
2023-09-26 20:46:53 +0700

    membuat project CI 3 Pustaka Booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index(){
+        echo "<h1> perkenalkan <h1>";
+        echo "Nama Saya Angga, Saya Tinggal Di Jakarta Barat, Hobi Saya menonton film";
+    }
+}
+
+
+
+?>
